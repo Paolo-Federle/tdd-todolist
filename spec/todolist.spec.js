54f487195953cf4cf30bccd80f1534118fedec01
Paolo Federle <federlepaolo95@gmail.com>
2022-11-01 19:10:15 +0100

    work in progress, still trying to understand some part of it

--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -1,1 +1,55 @@
+const { todoItem, createID, getTodo, allTodo, setComplete } = require('../src/todolist.js')
 
+describe ("todoItem", () => {
+    it("check if Item as an ID, a description and completed set as false", () => {
+        const result = todoItem("clean the windows")
+
+        expect(result.id).toBeDefined()
+        expect(result.description).toBeDefined()
+        expect(result.completed).toBe(false)
+
+    })
+})
+
+describe ("createID", () => {
+    it("return an ID", () => {
+        const result = createID()
+
+        expect(result).toBeDefined()
+    })
+})
+
+describe ("getTodo", () => {
+    it("return all the to do items", () => {
+        const todo = todoItem("clean the windows")
+        const gotAllTodo = getTodo()
+
+        expect(gotAllTodo).toBe(allTodo)
+    })
+})
+
+describe ("getTodo('incomplete')", () => {
+    it("return all incomplete toDo", () => {
+        const gotAllTodo = getTodo("incomplete")
+
+        expect(gotAllTodo).toEqual(allTodo)
+    })
+})
+
+describe ("getTodo('complete')", () => {
+    it("return all completed toDo", () => {
+        const gotAllTodo = getTodo("complete")
+
+        expect(gotAllTodo).toEqual([])
+    })
+})
+
+describe ("setComplete(id)", () => {
+    it("set a todo as complete", () => {
+        const todo = todoItem("clean the park")
+        console.log(todo)
+        const setCompleted = setComplete(todo.id)
+
+        expect(todo.completed).toBeTruthy()
+    })
+})
--- src/todolist.js
+++ src/todolist.js
@@ -1,1 +1,48 @@
+const allTodo = []
+let counter = 0
 
+function todoItem(descriptionText) {
+    const todoItem = {
+        id: createID(),
+        description: descriptionText,
+        completed: false
+    }
+    allTodo.push(todoItem)
+    return todoItem
+}
+
+
+function createID() {
+    counter++
+    return counter
+}
+
+
+function getTodo(filter) {
+    if (!filter) {
+        return allTodo
+    }
+    if (filter === "incomplete") {
+        const incompleteTodo = allTodo.filter(toDo => !toDo.completed)
+        return incompleteTodo
+    }
+    if (filter === "complete") {
+        const completeTodo = allTodo.filter(toDo => toDo.completed)
+        return completeTodo
+    }
+
+}
+
+function setComplete(idNumber){
+    for (const item of allTodo) {
+        console.log("id is", item.id)
+        if (item.id === idNumber) {
+            console.log("found ID")
+            return item.completed = true
+        }
+    }
+    // console.log(allTodo)
+    // console.log("didn't found ID")
+}
+
+module.exports = { todoItem, createID, getTodo, allTodo, setComplete }
